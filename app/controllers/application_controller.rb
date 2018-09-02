6720078b2213c3f3736e65799eae39e7813e6896
Wasif Khan <waasif.khaan@gmail.com>
2018-09-02 21:46:27 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,1 +1,43 @@
+class ApplicationController < Sinatra::Base
+  register Sinatra::ActiveRecordExtension
+  set :views, Proc.new { File.join(root, "../views/") }
 
+  configure do
+    enable :sessions
+    set :session_secret, "secret"
+  end
+
+  get '/' do
+    erb :home
+  end
+
+  get '/registrations/signup' do
+    erb :'/registrations/signup'
+  end
+
+  post '/registrations' do
+    @user = User.new(params)
+
+    redirect '/users/home'
+  end
+
+  get '/sessions/login' do
+    erb :'sessions/login'
+  end
+
+  post '/sessions' do
+
+    redirect '/users/home'
+  end
+
+  get '/sessions/logout' do 
+
+    redirect '/'
+  end
+
+  get '/users/home' do
+
+    erb :'/users/home'
+  end
+
+end
